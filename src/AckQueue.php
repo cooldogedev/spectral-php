0a099172645fc287f2587105afdcc4136d8f6b47
Doge <cooldogedev@gmail.com>
2024-10-11 13:16:32 +0300

    feat(Acknowledgement): sort sequence ids in case of an out-of-order delivery

--- src/AckQueue.php
+++ src/AckQueue.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace cooldogedev\spectral;
 
 use function count;
+use function sort;
 
 final class AckQueue
 {
+    private int $sequenceID = 0;
     private int $lastAck = 0;
+
+    private bool $sort = false;
 
     /**
      * @var array<int>
@@ -17,6 +21,10 @@ final class AckQueue
 
     public function add(int $sequenceID): void
     {
+        $this->sequenceID++;
+        if (!$this->sort && $sequenceID !== $this->sequenceID) {
+            $this->sort = true;
+        }
         $this->lastAck = Utils::unixNano();
         $this->queue[] = $sequenceID;
     }
@@ -29,7 +37,12 @@ final class AckQueue
         if (count($this->queue) === 0) {
             return null;
         }
+
         $queue = $this->queue;
+        if ($this->sort) {
+            sort($queue);
+        }
+        $this->sort = false;
         $this->queue = [];
         return [Utils::unixNano() - $this->lastAck, $queue];
     }
